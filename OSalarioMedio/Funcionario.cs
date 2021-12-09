d99d4aeefdeadaf5e2ab3ee3cc9a0d3773c1fc19
Daniel Haag <danni.haag91@gmail.com>
2021-12-09 18:27:14 -0300

    primeiro commit

---
+++ OSalarioMedio/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSalarioMedio
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+
+        public Funcionario LerDados()
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.WriteLine("Nome:");
+            funcionario.Nome = Console.ReadLine();
+            Console.WriteLine("Salario:");
+            funcionario.Salario = int.Parse(Console.ReadLine());
+
+            return funcionario;
+        }
+    }
+}
---
+++ OSalarioMedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSalarioMedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+            decimal salarioMedio = 0;
+
+            Console.WriteLine("Média de salários.");
+            Console.WriteLine("Dados do primeiro funcionário:");
+            funcionario1 = funcionario1.LerDados();
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            funcionario2 = funcionario2.LerDados();
+
+            salarioMedio = ((funcionario1.Salario + funcionario2.Salario) / 2);
+
+            Console.WriteLine("Salário médio: " + salarioMedio.ToString());
+            Console.Read();
+
+        }
+    }
+}
